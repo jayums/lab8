cd28c209230aefa5317d0bea9dc0f21ac531a885
James Guan <james.guan5693@gmail.com>
2014-02-27 10:02:02 -0800

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,7 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$('body').on('click', '.likeBtn', function(event) {
+		ga("send", "event", "like", "click");
+	});
 }
